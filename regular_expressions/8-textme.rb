875d624faf0816da25da4eaea91c16d9babf8882
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-09-15 04:20:20 +0200

    Update 8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -2,4 +2,4 @@
 sender = ARGV[0].scan(/(?<=[f]rom:).+(.(?=\]))/).join
 receiver = ARGV[0].scan(/(?<=[t]o:).+(.(?=\]))/).join
 flags = ARGV[0].scan(/(?<=[f]lags:).+(.(?=\]))/).join
-puts "#{sender},#{receiver},#{flags}"
+puts (sender + "," + receiver + "," + flags) 
