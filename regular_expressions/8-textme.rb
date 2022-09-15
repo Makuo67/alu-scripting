520d2659865db62457e17294a2b1d3cdc3c62c3e
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-09-15 04:35:58 +0200

    Update 8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 print ARGV[0].scan(/(?<=[f]rom:).+(.(?=\]))/).join
 print ","
-print ARGV[0].scan(/(?<=[t]o:).+(.(?=\]))/).join
+print ARGV[1].scan(/(?<=[t]o:).+(.(?=\]))/).join
 print ","
-print ARGV[0].scan(/(?<=[f]lags:).+(.(?=\]))/).join
+print ARGV[2].scan(/(?<=[f]lags:).+(.(?=\]))/).join
